4c3cdabf9520d929d47328b034d8cfadc316dc90
Auwal <auwaluyunusa1986@gmail.com>
2022-10-15 16:28:12 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,14 @@
 
 /**
  *
- *   main - Entry point
+ * main - Entry point
  *
  *   
  *
- *     Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  *
  *   
-    */
+ */
 
              int main(void)
 
