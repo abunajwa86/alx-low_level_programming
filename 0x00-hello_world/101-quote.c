c6886c402bcbcee6780e1178cf1965b80e8b79d3
Auwal <auwaluyunusa1986@gmail.com>
2022-10-16 07:07:27 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include<unistd.h>
 
-/** 
+/**
  * main - Entry point
  *
  * Description: print a quote using write function
  *   ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: This time we return an error 1. 
+ * Return: This time we return an error 1.
  */
 
 int main(void)
