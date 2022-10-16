932e65e4030872fa2da515e5d97c1004a88b1377
Auwal <auwaluyunusa1986@gmail.com>
2022-10-16 04:02:06 -0700

    task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 /** 
  * main - prints exactly "and that piece of art is useful"
  *
- * - Dora Korpar, 2015-10-19",
+ * - "Dora Korpar, 2015-10-19",
  *
  * followed by a new line, to the standard error.
  *
@@ -13,18 +13,10 @@
  *
  */
 
-
-
-
-
-
-
 int main(void)
 
-
-
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+		write('2', "and that piece of art is useful\" - Dora Korpar, '2015-10-19'\n" '59');
 
 		return (1);
 
