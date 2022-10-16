dbdf12ad6372f9d7be9d15e2132c5d11cb063dfb
Auwal <auwaluyunusa1986@gmail.com>
2022-10-16 03:37:23 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,7 @@
 
 #include <unistd.h>
 
-/**
- *
+/** 
  * main - Entry Points
  *
  * Description: C program to print exactly Dora Korpar, 2015-10-19",
@@ -23,8 +22,7 @@ int main(void)
 
 
 {
-
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",);
 
 		return (1);
 
