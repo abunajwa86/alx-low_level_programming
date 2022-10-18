2471e5fec7506d0a9143366de472cf75cf0a8c44
Auwal <auwaluyunusa1986@gmail.com>
2022-10-18 07:01:55 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program to print Alphabet in lower and upper case.
+ *
+ * Description: using main function to print Alphabet
+ *
+ * Return: 0
+ * */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+}
