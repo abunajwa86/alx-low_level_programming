a589b3436ff6ee4ae5b027398c0a15467db08308
Auwal <auwaluyunusa1986@gmail.com>
2022-10-18 06:53:45 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,10 +28,11 @@ int main(void)
 	{
 		printf("last digit of %d is %d and is 0\n", n, l);
 }
-        else 
+        else
 	{
 		printf("last digit %d is %d and is less than 6 and not 0\n", n, l);
 }
 
 	return (0);
 }
+
