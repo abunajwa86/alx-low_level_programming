bc38ba89836cfb000f265550ce2ee5a299bf9b1c
Auwal <auwaluyunusa1986@gmail.com>
2022-10-19 08:54:20 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,10 +8,9 @@
 int main(void)
 {
 	int num;
-	for (num = 0; num < 10; num++)
+	for (num = 0; num < 10; num++;\n")
 	{
-		printf("%i", num);
+		printf("% i", num);
 }
-	printf("\n");
 	return (0);
 }
