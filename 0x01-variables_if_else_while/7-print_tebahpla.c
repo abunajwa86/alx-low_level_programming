4dac25f0c5252b5331cbe848b81e13063db2a9a7
Auwal <auwaluyunusa1986@gmail.com>
2022-10-19 13:59:27 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-	char(ch);
+	char ch;
 {
 	for (ch = 'z'; ch = 'a'; ch--)
-	{
-		putchar(ch)
+
+		putchar(ch);
 {
 	putchar('\n');
+}
 	return (0);
 }
