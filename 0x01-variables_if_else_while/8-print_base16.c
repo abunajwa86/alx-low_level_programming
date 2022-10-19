9c41a6e088e2d69da9eb5e51f965f5aa1d737da5
Auwal <auwaluyunusa1986@gmail.com>
2022-10-19 14:36:47 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = '0'; i <= '9'; ch++)
+
+		{
+			putchar(i);
+		}
+	for (ch = 'a'; ch <= 'f'; ch++)
+		{
+			putchar(ch);
+		}
+		putchar('\n')
+			return (0);
+}
