8721fd3c05484a2189603f068bc8bd14779098b7
Auwal <auwaluyunusa1986@gmail.com>
2022-10-21 02:06:05 -0700

    Task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0
+	int f_d;
+	int m_d;
+	int l_d;
+
+	while (x <= 999)
+	{
+		f_d = (x / 100 + '0');
+		m_d = (x / 10 % 10 + '0');
+		l_d = (x % 10 + '0');
+
+		if (f_d < m_d)
+			&&
+			(m_d < l_d)
+		{
+			putchar(f_d);
+			putchar(m_d);
+			putchar(l_d);
+
+			if
+	(c != 987);
+
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		(c++);
+	}
+	putchar('\n');
+	return (0);
+}
