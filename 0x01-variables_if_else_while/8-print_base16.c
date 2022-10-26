0029fc05d88770eeac3c5e0bb7b70d60e208d5a0
Auwal Yunusa Hassan <113687027+abunajwa86@users.noreply.github.com>
2022-10-26 07:19:12 -0700

    8th

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success/Correct)
- * /
- int main(void)
-	
- {
- 	char base_16;
-	for (char base_16 = 48; base_16 <= 57; base_16++)
-	{
-	putchar(base_16);
-	}
-	for (base_16 = 97; base_16 <= 102; base_16++)
-	{
-	putchar(base_16);
-	}
-	putchar('\n');
-	return (0);
-} 
+* main - Entry point
+*
+*Return: Always 0 (success/correct)
+*/
+int main(void)
+
+{
+
+char base_16;
+
+for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16);
+}
+
+for (base_16 = 97; base_16 <= 102; base_16++)
+{
+putchar(base_16);
+}
+
+putchar('\n');
+return (0);
+}
